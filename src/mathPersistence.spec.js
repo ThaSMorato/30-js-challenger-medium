d12013a9e9423ad4ca6e6cd4922f5cc70b5e69fc
Thales Morato <tha.s.morato@gmail.com>
2022-01-12 12:11:13 -0300

    refact: adjust descriptions

--- src/mathPersistence.spec.js
+++ src/mathPersistence.spec.js
@@ -3,7 +3,7 @@ import { describe, it, expect } from "@jest/globals";
 import { mathPersistence } from "./mathPersistence";
 
 describe("Math persistence", () => {
-  it("should return 3 to 539 with", () => {
+  it("should return 3 to 539", () => {
     const content = 539;
     const result = mathPersistence(content);
     const expected = 3;
@@ -11,7 +11,7 @@ describe("Math persistence", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should return 4 to 999 with", () => {
+  it("should return 4 to 999", () => {
     const content = 999;
     const result = mathPersistence(content);
     const expected = 4;
@@ -19,7 +19,7 @@ describe("Math persistence", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should return 0 to 7 with", () => {
+  it("should return 0 to 7", () => {
     const content = 7;
     const result = mathPersistence(content);
     const expected = 0;
@@ -27,7 +27,7 @@ describe("Math persistence", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should return 5 to 7169 with", () => {
+  it("should return 5 to 7169", () => {
     const content = 7169;
     const result = mathPersistence(content);
     const expected = 5;
